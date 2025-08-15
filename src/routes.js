710e918b9bdbce7244f971c5e8d06f44336d9b93
kreker36barabashka <tackabiog@hotmail.com>
2025-08-15 00:27:14 -0500

    feat: initial commit for src/routes.js

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const dashboard = require('./dashboard');
+
+const router = express.Router();
+
+router.use('/dashboard', dashboard);
+
+module.exports = router;
