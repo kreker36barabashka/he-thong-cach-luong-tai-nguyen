a648367c87767dbfe186e6f3ba60bbf5072a7ba9
kreker36barabashka <tackabiog@hotmail.com>
2025-08-15 00:27:09 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/resource_monitor', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('Dashboard tài nguyên');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
